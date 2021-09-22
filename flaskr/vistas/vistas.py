01b3ce04ccb0b98e808c1a187db32d13c32fd2a6
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-09-22 11:21:48 -0500

    correcion develop

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -4,8 +4,7 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 import sys
-from datetime import datetime
-import json
+
 
 cancion_schema = CancionSchema()
 usuario_schema = UsuarioSchema()
